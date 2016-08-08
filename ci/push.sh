75a53f09b6ad5ef85c74b961df07e9e43264d23f
lakshman29 <lakshman29@gmail.com>
2016-08-08 15:42:22 +0530

    Update push.sh

--- ci/push.sh
+++ ci/push.sh
@@ -3,16 +3,18 @@
 set -e # fail fast
 set -x # print commands
 #mvn archetype:generate -Dfilter=org.apache.maven.archetypes:maven-archetype-quickstart
-mvn archetype:generate -DgroupId=com.test -DartifactId=DEF -Dpackaging=jar -Dversion=1.0 -DinteractiveMode=false
+curl -u 'lakshman29@gmail.com:Test@123' https://api.github.com/user/repos -d '{"name":"resource1"}'
+
 git init
+mvn archetype:generate -DgroupId=com.test -DartifactId=DEF -Dpackaging=jar -Dversion=1.0 -DinteractiveMode=false
 
 git add .
 git commit -m "pushing the repo"
 #git remote add origin https://github.com/lakshman29/practice-web.git
 
-curl -u 'lakshman29@gmail.com:Test@123' https://api.github.com/user/repos -d '{"name":"resource"}'
+
 # Remember replace USER with your username and REPO with your repository/application name!
-git remote add origin git@github.com:lakshman29@gmail.com/resource.git
+git remote add origin git@github.com:lakshman29@gmail.com/resource1.git
 git push origin master
 
 
